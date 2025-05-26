48b3083a1e7e2d64b62eee49223a7133ef9b8d83
Alexolebo <114154851+Alexolebo@users.noreply.github.com>
2025-05-26 14:29:42 +0200

    Ajout Class Item + test
    Permet de créer des objects

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,7 +1,5 @@
 package main;
 
-
-
 import utils.Color;
 import utils.StringStyling;
 import utils.Style;
@@ -9,14 +7,20 @@ import utils.Style;
 public class Main {
 
     public static void main(String[] args) {
-    
-       
+
         System.out.println(StringStyling.StyleString("Starting...", Style.ITALIC, Color.BLACK));
         Game game = new Game();
+        // Création d’un objet Item
+        Item cle = new Item("Clé", "Une clé dorée ancienne.");
+
+        // Affichage pour vérifier que tout fonctionne
+        System.out.println("Nom de l'objet : " + cle.getName());
+        System.out.println("Description de l'objet : " + cle.getDescription());
+        System.out.println("toString() : " + cle);
+
         game.run();
+
         System.out.println(StringStyling.StyleString("Terminating...", Style.ITALIC, Color.BLACK));
     }
 
 }
-
-
