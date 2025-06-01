7ade1488073c66b735de70167cf6ce11c69c798a
Sacckke <mmulugeta47@gmail.com>
2025-06-01 18:35:30 +0200

    Update LookCommand.java

--- src/main/LookCommand.java
+++ src/main/LookCommand.java
@@ -7,22 +7,18 @@ public class LookCommand implements ICommand {
         this.player = player;
     }
 
-    @Override
     public String getDescription() {
         return "Show the description of the current location.";
     }
 
-    @Override
     public String getUsage() {
         return "look";
     }
 
-    @Override
     public boolean hasValidArguments(String input) {
         return input.trim().equals("look");
     }
 
-    @Override
     public void execute(String input, Game game) {
         Location current = player.getLocation();
         System.out.println(current.getDescription());
