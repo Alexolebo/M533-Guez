c8d1a49eab7f50f7c488f3d19aa6d24b8ab987ee
Sacckke <mmulugeta47@gmail.com>
2025-06-01 18:31:09 +0200

    Create GameObject.java

---
+++ src/main/GameObject.java
@@ -0,0 +1,28 @@
+// Source code is decompiled from a .class file using FernFlower decompiler.
+package main;
+
+public class GameObject {
+   private String name;
+   private String description;
+
+   public GameObject(String name, String description) {
+      this.name = name;
+      this.description = description;
+   }
+
+   public String getName() {
+      return this.name;
+   }
+
+   public String getDescription() {
+      return this.description;
+   }
+
+   public void inspect() {
+      System.out.println("You see " + this.description);
+   }
+
+   public void use(Player player, Game game) {
+      System.out.println("Nothing happens.");
+   }
+}
